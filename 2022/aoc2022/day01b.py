37f3173ffad56bc6aba2b557d3ec77c98d95fbe9
Cole Maclean <hi@colemaclean.dev>
2022-12-03 08:58:05 -0800

    Day 1, part b

--- 2022/aoc2022/day01b.py
+++ 2022/aoc2022/day01b.py
@@ -4,22 +4,22 @@ from io import StringIO
 INPUT_FILE = pathlib.Path(pathlib.Path(__file__).parent.parent, "data/input_01.txt")
 
 
-def max_calorie_counter(calories: StringIO) -> int:
+def top_3_calorie_counter(calories: StringIO) -> int:
+    elf_calories = []
     current_elf_counter = 0
-    max_calories_carried = 0
     for line in calories:
         # Empty string means next elf
         if line == "\n":
-            max_calories_carried = max(max_calories_carried, current_elf_counter)
+            elf_calories.append(current_elf_counter)
             current_elf_counter = 0
         else:
             current_elf_counter += int(line.rstrip("\n"))
 
-    max_calories_carried = max(max_calories_carried, current_elf_counter)
+    elf_calories.append(current_elf_counter)
 
-    return max_calories_carried
+    return sum(sorted(elf_calories, reverse=True)[:3])
 
 
 if __name__ == "__main__":
     input = StringIO(INPUT_FILE.read_text())
-    print(max_calorie_counter(input))
+    print(top_3_calorie_counter(input))
--- 2022/tests/test_day01.py
+++ 2022/tests/test_day01.py
@@ -1,5 +1,6 @@
 from io import StringIO
-from aoc2022.day01a import calorie_counter
+from aoc2022.day01a import max_calorie_counter
+from aoc2022.day01b import top_3_calorie_counter
 
 TEST_DATA = """1000
 2000
@@ -17,5 +18,9 @@ TEST_DATA = """1000
 10000"""
 
 
-def test_calorie_counter():
-    assert calorie_counter(StringIO(TEST_DATA)) == 24000
+def test_max_calorie_counter():
+    assert max_calorie_counter(StringIO(TEST_DATA)) == 24000
+
+
+def test_top_3_calorie_counter():
+    assert top_3_calorie_counter(StringIO(TEST_DATA)) == 45000
