78f279f5ce6965b9a868833da3dc4d673029a88c
Cole Maclean <hi@colemaclean.dev>
2023-11-30 22:04:33 -0800

    add input downloader

---
+++ dlinput.py
@@ -0,0 +1,20 @@
+# Source: https://github.com/jonathanpaulson/AdventOfCode/blob/master/get_input.py
+
+import argparse
+import os
+import subprocess
+import sys
+
+# Usage: AOC_SESSION=... python dlinput.py 1 > 2023/day01.in
+
+SESSION = os.environ['AOC_SESSION']
+
+parser = argparse.ArgumentParser(description='Read input')
+parser.add_argument('day', type=int)
+args = parser.parse_args()
+
+cmd = f'curl -s https://adventofcode.com/2023/day/{args.day}/input --cookie "session={SESSION}"'
+output = subprocess.check_output(cmd, shell=True)
+output = output.decode('utf-8')
+print(output, end='')
+print('\n'.join(output.split('\n')[:10]), file=sys.stderr)
